dc4a704eda8428e30c6fab34243d1bd55b51ccbc
Mikunj <mikunj@live.com.au>
2019-07-09 14:49:18 +1000

    Fix incorrect config being passed to LokiClient.

--- api/core/loki.js
+++ api/core/loki.js
@@ -4,16 +4,17 @@ import { clients } from 'bridge-core';
 const { filename, password: walletPassword, accountIndex } = config.get('loki.wallet');
 const { host, port, username, password: rpcPassword } = config.get('loki.walletRPC');
 
-export default new clients.LokiClient({
-  rpc: {
-    hostname: host,
-    port,
-    username,
-    password: rpcPassword,
-  },
-  wallet: {
-    filename,
-    password: walletPassword,
-    accountIndex,
-  },
-});
+const rpc = {
+  hostname: host,
+  port,
+  username,
+  password: rpcPassword,
+};
+
+const wallet = {
+  filename,
+  password: walletPassword,
+  accountIndex,
+};
+
+export default new clients.LokiClient(rpc, wallet);
