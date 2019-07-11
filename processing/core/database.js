6aad7f10426b28b12ddb60ec37a9d1c9ae6ebcff
Mikunj Varsani <mikunj@live.com.au>
2019-07-12 09:09:00 +1000

    Added SSL property to database config. Increased test timeout.

--- processing/core/database.js
+++ processing/core/database.js
@@ -1,7 +1,7 @@
 import config from 'config';
 import { Database, clients } from 'bridge-core';
 
-const { host, port, database, user, password } = config.get('database');
+const { host, port, database, user, password, ssl } = config.get('database');
 
-export const postgres = new clients.PostgresClient({ host, port, database, user, password });
+export const postgres = new clients.PostgresClient({ host, port, database, user, password, ssl });
 export const db = new Database(postgres);
